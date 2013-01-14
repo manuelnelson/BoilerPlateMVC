62120464cef7cc8a0f02f3d9f16178c6e6913446
manuelnelson <elnels@gmail.com>
2013-01-14 09:39:54 -0600

    Fixed references
    Primed app for unit testing as well

---
+++ src/Application.BusinessLogic.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.BusinessLogic.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
