2e33c77a479c7b2456016fefcd290cdbd2c2d6ec
Emmanuel Nelson <elnels@gmail.com>
2013-10-04 15:32:53 -0500

    lordy

--- src/Application.Tests/Web.Tests/HomeControllerTest.cs
+++ src/Application.Tests/Web.Tests/HomeControllerTest.cs
@@ -19,31 +19,5 @@ namespace Application.Tests.Web.Tests
             // Assert
             Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
     }
 }
--- src/Application.Web/Controllers/HomeController.cs
+++ src/Application.Web/Controllers/HomeController.cs
@@ -11,18 +11,5 @@ namespace Application.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
